def3c6570f254dcdceb69075750936967d50f0be
JuliaHeleneW <jwerner@csumb.edu>
2016-09-23 13:40:06 -0700

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,5 +1,8 @@
 #screenshot in Python
 import pyscreenshot as ImageGrab
+#import for video
+import numpy as np
+import cv2
 #fullscreen
 im=imageGrab.grab()
 im.show()
@@ -11,3 +14,6 @@ ytwo=raw_input("Enter Y2:")
 im=imageGrab.grab(bbox=(%xone,%yone,%xtwo,%ytwo))
 #to file
 ImageGrab.grab_to_file('im.png')
+#capture frames from the screen
+cv2.VideoWriter.open(filename, fourcc, fps, frameSize[, isColor])
+#fourcc: http://www.fourcc.org/codecs.php
